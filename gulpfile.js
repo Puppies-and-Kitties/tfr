03619f0a54e87fc0a8dcc1f4954df8e364ac4cf9
John Peterson <jlpeterson829@gmail.com>
2015-04-01 23:29:26 -0400

    Add test folder with controller/service tests karma, jasmine, update gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -6,10 +6,20 @@ var sass = require('gulp-sass');
 var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
 var sh = require('shelljs');
+var karma = require('karma').server;
 
 var paths = {
   sass: ['./scss/**/*.scss']
 };
+
+gulp.task('test', function(done) {
+  karma.start({
+    configFile: __dirname + '/tests/my.conf.js',
+    singleRun: true
+  }, function() {
+    done();
+  });
+});
 
 gulp.task('default', ['sass']);
 
