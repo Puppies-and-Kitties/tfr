54948180ba4678cf16fd557f355ec3bb086f979a
Daniel Miller <danielmiller.nl@gmail.com>
2015-04-06 18:25:15 -0230

    started adding factories and controllers

---
+++ www/swipe/controller.js
@@ -0,0 +1,23 @@
+angular.module('swipe.controllers', [])
+
+.controller('SwipeController', function($scope,$timeout,Candidates,User) {
+  $scope.candidates = Candidates.all();
+
+  $scope.currentCandidate = angular.copy($scope.candidates[0]);
+
+  $scope.sendFeedback = function (match){
+
+    if (match) {
+      User.addCandidateToMatches($scope.currentCandidate);
+    }
+
+    $scope.currentCandidate.rated = match;
+    $scope.currentCandidate.hide = true;
+
+    $timeout(function(){
+      var randomCandidate = Math.round(Math.random() * ($scope.candidates.length-1)) ;
+      $scope.currentCandidate = angular.copy($scope.candidates[randomCandidate]);
+    },250);
+
+  };
+})
