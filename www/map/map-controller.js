13f3bc7c2c01d5d75e01e2e4e6b19491d4481269
dblanchard13 <davidb.prof@gmail.com>
2015-04-10 18:34:14 -0400

    (feat) Place a map marker on click

--- www/map/map-controller.js
+++ www/map/map-controller.js
@@ -32,36 +32,35 @@ angular.module('map.controllers', [])
 // })
 
 .controller('MapCtrl', function($scope, $ionicLoading){
-  var map;
-
-  $scope.initialize = function() {
-    console.log('running func')
-    var myLatlng = new google.maps.LatLng(37.3000, -120.4833);
-
-    var mapOptions = {
-        center: myLatlng,
-        zoom: 16,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-
-    map = new google.maps.Map(document.getElementById("map"), mapOptions);
-
-    navigator.geolocation.getCurrentPosition(function(pos) {
-        map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
-        var myLocation = new google.maps.Marker({
-            position: new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude),
-            map: map,
-            title: "My Location"
-        });
-    });
-
-    $scope.map = map;
-  };
+  var map, marker, markersArray = [];
 
   $scope.input = {
     address: null
   };
 
+  $scope.initialize = function() {
+    var myLatlng = new google.maps.LatLng(37.3000, -120.4833);
+
+    var mapOptions = {
+        center: myLatlng,
+        zoom: 13,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+    google.maps.event.addListener(map, "click", function(event){
+      // place a marker
+      placeMarker(event.latLng);
+    });
+
+    // Set the maps center to user's current position
+    navigator.geolocation.getCurrentPosition(function(pos) {
+        map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+    });
+
+    $scope.map = map;
+  };
 
   $scope.codeIt = function(){
     var geocoder = new google.maps.Geocoder();
@@ -72,15 +71,36 @@ angular.module('map.controllers', [])
       if (status == google.maps.GeocoderStatus.OK) {
         console.log('OK Status - ', results);
         map.setCenter(results[0].geometry.location);
-        var marker = new google.maps.Marker({
-            map: map,
-            position: results[0].geometry.location
-        });
-      } else {
+        placeMarker(results[0].geometry.location);
+      } 
+      else {
         alert('Geocode was not successful for the following reason: ' + status);
       }
     });
 
+  }
+
+  var placeMarker = function(location) {
+    // first remove all markers if there are any
+    deleteOverlays();
+
+    var marker = new google.maps.Marker({
+        position: location, 
+        map: map
+    });
+
+    // add marker in markers array
+    markersArray.push(marker);
+  }
+
+  // Deletes all markers in the array by removing references to them
+  var deleteOverlays = function() {
+    if (markersArray) {
+        for (i in markersArray) {
+            markersArray[i].setMap(null);
+        }
+    markersArray.length = 0;
+    }
   }
 
   google.maps.event.addDomListener(window, 'load', $scope.initialize());
