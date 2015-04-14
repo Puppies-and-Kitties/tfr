f20e5696a8dec8ea4604eaf8638cbf26b039650b
dblanchard13 <davidb.prof@gmail.com>
2015-04-13 23:11:27 -0400

    (refactor) Remove openFb library and callbacks

--- www/login/controller.js
+++ www/login/controller.js
@@ -2,20 +2,7 @@ angular.module('login.controllers', [])
 
 .controller('LoginCtrl', function($scope, $state, LoginFact, FIREBASE_REF, $firebaseSimpleLogin, userSession){
   
-  $scope.fbLogin = function(arg){
-    openFB.login(function(response){
-      if(response.status === 'connected'){
-        console.log('Facebook login succeeded');
-        
-        // Redirect to tab.dash
-        $state.go('tab.account');
-      }
-      else {
-        alert('Facebook login failed');
-      }
-    },
-    {scope: 'email, publish_actions, public_profile'});
-  };
+  // {scope: 'email, publish_actions, public_profile'});
 
   userSession.auth=$firebaseSimpleLogin(new Firebase(FIREBASE_REF));
 
