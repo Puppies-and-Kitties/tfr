bac81d62ff9108ae7222b458f9f858530b76e4f2
Daniel Miller <danielmiller.nl@gmail.com>
2015-04-16 01:20:29 -0230

    Match specific chats now enabled. User's name and photo properly posted

--- www/chats/controller.js
+++ www/chats/controller.js
@@ -1,11 +1,19 @@
 angular.module('chats.controllers', [])
 
-.controller('ChatsCtrl', function($scope, User, Chats) {
-  $scope.chats = Chats.all();
+.controller('ChatsCtrl', function($scope, User, Chats, userSession, $stateParams) {
+
+  // We will have to refactor this controller and other parts of the app as to:
+    // When a user A likes user B, we check if B already likes A
+      // If so, its a match and the url for the firebase chat will already be stored with B's like data
+      // If not, the url will have to be created at this time and stored with A's like data
+    // Once this url is stored, scope.chat (which will only be defined in the case of matches)
+    // will only need to take one parameter: the url for the firebase chat
+
+  $scope.chats = Chats.set(userSession.user.id,$stateParams.matchId);
   $scope.remove = function(chat) {
     Chats.remove(chat);
   }
-  $scope.add = function(from,chat){
-    Chats.add(from,chat);
+  $scope.add = function(message){
+    Chats.add($scope.chats,User.name,message,User.fbid);
   }
 });
--- www/chats/services.js
+++ www/chats/services.js
@@ -1,28 +1,31 @@
 angular.module('chats.services', ['firebase'])
 
 .factory('Chats', function($firebaseArray,userSession,$stateParams) {
-  console.log('userSession',userSession);
-  var currentMatchChats = new Firebase("https://ionictestchat.firebaseio.com/"+userSession.user.id+$stateParams.matchId);
-
-  var chats = $firebaseArray(currentMatchChats);
 
   return {
-    all: function() {
-      return chats;
+    // Currently not being used
+    // all: function() {
+    //   return chats;
+    // },
+    // remove: function(chat) {
+    //   chats.splice(chats.indexOf(chat), 1);
+    // },
+    // get: function(chatId) {
+    //   for (var i = 0; i < chats.length; i++) {
+    //     if (chats[i].id === parseInt(chatId)) {
+    //       return chats[i];
+    //     }
+    //   }
+    //   return null;
+    // },
+    set: function(userId,matchId){
+      var currentMatchChats = new Firebase("https://ionictestchat.firebaseio.com/"+userId+matchId);
+      console.log('currentMatchChats',currentMatchChats);
+      return $firebaseArray(currentMatchChats);
     },
-    remove: function(chat) {
-      chats.splice(chats.indexOf(chat), 1);
-    },
-    get: function(chatId) {
-      for (var i = 0; i < chats.length; i++) {
-        if (chats[i].id === parseInt(chatId)) {
-          return chats[i];
-        }
-      }
-      return null;
-    },
-    add: function(from,message){
-      chats.$add({from:from,message:message});
+    add: function(firebaseArr,from,message,fbId){
+      var photoUri = "//graph.facebook.com/"+fbId+"/picture";
+      firebaseArr.$add({from:from,message:message,photoUri:photoUri});
     }
   };
 });
