4601e22c67bc25edce48d40961727fb421f4d594
dblanchard13 <davidb.prof@gmail.com>
2015-04-18 17:51:10 -0400

    (fix) MongoLab Environment Testing

--- server/config.js
+++ server/config.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 
 var app = express();
 
-var mongoURI = process.env.PROD_MONGODB || process.env.MONGO_URI || 'mongodb://localhost/roome';
+var mongoURI = process.env.MONGOLAB_URI || 'mongodb://localhost/roome';
 
 mongoose.connect(mongoURI);
 var db = mongoose.connection;
