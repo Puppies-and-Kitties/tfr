c4c25ac266ada906e308a280b25623bd0a26f82c
dblanchard13 <davidb.prof@gmail.com>
2015-04-24 02:07:52 -0700

    (cleanup) Delete frivolous comments from chats directory

--- www/chats/controller.js
+++ www/chats/controller.js
@@ -2,21 +2,12 @@ angular.module('chats.controllers', [])
 
 .controller('ChatsCtrl', function($scope, User, Chats, MatchesFactory, userSession, $stateParams) {
 
-  // We will have to refactor this controller and other parts of the app as to:
-    // When a user A likes user B, we check if B already likes A
-      // If so, its a match and the url for the firebase chat will already be stored with B's like data
-      // If not, the url will have to be created at this time and stored with A's like data
-    // Once this url is stored, scope.chat (which will only be defined in the case of matches)
-    // will only need to take one parameter: the url for the firebase chat
-
   var matchId = $stateParams.matchId;
   console.log('match id in chats controller - ', $stateParams)
   
   $scope.match = MatchesFactory.get(matchId);
 
-  var chatURL = $scope.match.matched[User._id] || User.matched[$scope.match._id];// $scope.existingChatURL = "https://ionictestchat.firebaseio.com/10155475481120094499"
-
-  
+  var chatURL = $scope.match.matched[User._id] || User.matched[$scope.match._id];  
 
   if(!chatURL){
     console.log("going to set chat url")
